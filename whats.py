95f580fbaa143b49755fecc1d85481507d7f5c43
W.Hirusha Rukshan Fernando <87813313+Dark-Knight-Hiruwa@users.noreply.github.com>
2022-02-14 19:24:34 +0530

    Whatsapp Hacke ♥️

---
+++ whats.py
@@ -0,0 +1,180 @@
+#code by hiruwa
+#WHATSAPP HACK
+#OWNER HIRUWA
+
+import sys, time, socket, threading, os
+
+class color:
+    blue = '\033[94m'
+    cyan = '\033[96m'
+    green = '\033[92m'
+    red = '\033[31m'
+    pink = '\033[35m'
+    green = '\033[1;32m'
+
+
+def printf(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(1 / 100)
+        
+def prints(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(1 / 10)
+        
+        
+tp = ["182 839", "828 727", "828 626","627 829","728 019","725 018","527 829"]
+
+tp1 = tp[0]
+tp2 = tp[1]
+tp3 = tp[2]
+tp4 = tp[3]
+tp5 = tp[4]
+tp6 = tp[5]
+tp7 = tp[6]
+
+banner = color.green  + """
+__        ___   _    _  _____ ____    _   _    _    ____ _  __
+\ \      / / | | |  / \|_   _/ ___|  | | | |  / \  / ___| |/ /
+ \ \ /\ / /| |_| | / _ \ | | \___ \  | |_| | / _ \| |   |   /
+  \ V  V / |  _  |/ ___ \| |  ___) | |  _  |/ ___ \ |___| . \ 
+   \_/\_/  |_| |_/_/   \_\_| |____/  |_| |_/_    \_\____|_|\_\ \n \n                                                         V1.0.0"""
+
+line = """
+====================================================="""
+
+space = """
+
+
+
+"""
+
+load = """
+
+▇
+▇▇
+▇▇▇
+▇▇▇▇
+▇▇▇▇▇
+▇▇▇▇▇▇
+▇▇▇▇▇▇▇
+▇▇▇▇▇▇▇▇
+▇▇▇▇▇▇▇▇▇
+▇▇▇▇▇▇▇▇▇▇
+▇▇▇▇▇▇▇▇▇▇▇"""
+printf(banner)
+printf("<<< TOOL BY HIRUWA >>>".center(70))
+printf("<<< T-REX WA BOT >>>".center(70))
+printf("<<< V1.0.0 >>>".center(70))
+print("")
+printf(line)
+print(color.pink + space)
+option = input("YOU WANT TO ANYONE WHATSAPP ACCOUNT ( Y / N ) :- ")
+
+if option == 'Y' :
+	os.system("clear")
+	
+	printf(banner)
+	
+	number = input("ENTER YOUR TARGET PHONE NUMBER ( 94********* )  :-  ")
+	os.system("clear")
+	print(space)
+	printf(color.red + number +  "IS SERCHING TO ATTACK ............")
+	time.sleep(2)
+	prints(load)
+	time.sleep(4)
+	print(space)
+	printf(number + "DETECT THIS NUMBER.........."+space+color.green+"SUCCESFULLY DETECTED....................")
+	print(space)
+	
+	otp = input("NOW YOU OPEN NEW WHATSAPP ACCOUNT AND TRY TO LOG "+number+"THIS NUMBER AND CLICK ENTER TO GET OTP CODE.... :- ")
+	os.system("clear")
+	printf(color.green + space)
+	printf(number)
+	printf(tp1)
+	time.sleep(2)
+	print(space)
+	printf(number)
+	printf(tp2)
+	time.sleep(2)
+	print(space)
+	printf(number)
+	printf(tp3)
+	time.sleep(2)
+	print(space)
+	printf(number)
+	printf(tp4)
+	print(space)
+	time.sleep(2)
+	printf(number)
+	printf(tp5)
+	print(space)
+	time.sleep(2)
+	printf(number)
+	printf(tp6)
+	print(space)
+	time.sleep(2)
+	printf(number)
+	printf(tp7)
+	print(space)
+	time.sleep(5)
+	printf(color.green+"😂😂😂 THIS IS FAKE 😂 ARE YOU CRAZY 😂 HOW HACK WHATSAPP IT HAS MORE SECURITY 😂😂😂😂")
+	
+if option == 'y':
+	os.system("clear")
+	
+	printf(banner)
+	
+	number = input("ENTER YOUR TARGET PHONE NUMBER ( 94********* )  :-  ")
+	os.system("clear")
+	print(space)
+	printf(color.red + number +  "IS SERCHING TO ATTACK ............")
+	time.sleep(2)
+	prints(load)
+	time.sleep(4)
+	print(space)
+	printf(number + "DETECT THIS NUMBER.........."+space+color.green+"SUCCESFULLY DETECTED....................")
+	print(space)
+	
+	otp = input("NOW YOU OPEN NEW WHATSAPP ACCOUNT AND TRY TO LOG "+number+"THIS NUMBER AND CLICK ENTER TO GET OTP CODE.... :- ")
+	os.system("clear")
+	printf(color.green + space)
+	printf(number)
+	printf(tp1)
+	time.sleep(2)
+	print(space)
+	printf(number)
+	printf(tp2)
+	time.sleep(2)
+	print(space)
+	printf(number)
+	printf(tp3)
+	time.sleep(2)
+	print(space)
+	printf(number)
+	printf(tp4)
+	print(space)
+	time.sleep(2)
+	printf(number)
+	printf(tp5)
+	print(space)
+	time.sleep(2)
+	printf(number)
+	printf(tp6)
+	print(space)
+	time.sleep(2)
+	printf(number)
+	printf(tp7)
+	print(space)
+	time.sleep(5)
+	printf(color.green+"😂😂😂 THIS IS FAKE 😂 ARE YOU CRAZY 😂 HOW HACK WHATSAPP IT HAS MORE SECURITY 😂😂😂😂")
+	
+else :
+	os.system("exit")
+	
+
+	
+
